4ce6d296c1231c137fd168526cca0989d58312c1
Matthew Matta <matthewlmatta@gmail.com>
2021-06-06 02:11:55 -0400

    moved to leaderboard

--- client/src/App.js
+++ client/src/App.js
@@ -5,90 +5,10 @@ import Leaderboard from './Pages/Leaderboard';
 import { Switch, Route } from 'react-router-dom';
 //import Login from './Pages/LoginForm';
 
-import {
-  BarChart,
-  Bar,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  Legend
-} from "recharts";
-
-const data = [
-  {
-    name: "Laurelwood",
-    Garbage: 4000,
-    Recycling: 2400,
-    Compost: 2400
-  },
-  {
-    name: "Westvale",
-    Garbage: 3000,
-    Recycling: 1398,
-    Compost: 2210
-  },
-  {
-    name: "Beechwood",
-    Garbage: 2000,
-    Recycling: 9800,
-    Compost: 2290
-  },
-  {
-    name: "Westmount",
-    Garbage: 2780,
-    Recycling: 3908,
-    Compost: 2000
-  },
-  {
-    name: "Vista Hills",
-    Garbage: 1890,
-    Recycling: 4800,
-    Compost: 2181
-  },
-  {
-    name: "Lakeshore",
-    Garbage: 2390,
-    Recycling: 3800,
-    Compost: 2500
-  },
-  {
-    name: "Eastbridge",
-    Garbage: 3490,
-    Recycling: 4300,
-    Compost: 2100
-  }
-];
 
 function App() {
   return (
-    <div className="app">
-      <Navbar />
-      <Switch>
-        <Route exact path="/" component={Home} />
-				<Route path="/data_visualization" component={Leaderboard} />
-      </Switch>
-      <BarChart
-        width={1400}
-        height={750}
-        data={data}
-        margin={{
-          top: 20,
-          right: 30,
-          left: 20,
-          bottom: 5
-        }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Bar dataKey="Garbage" stackId="a" fill="#8884d8" />
-        <Bar dataKey="Recycling" stackId="a" fill="#82ca9d" />
-        <Bar dataKey="Compost" stackId="a" fill="#c2e0e3" />
-      </BarChart>
-    </div>
+    
   );
 }
 
