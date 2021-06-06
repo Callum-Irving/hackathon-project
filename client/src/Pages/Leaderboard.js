a90d4b060ab21d95e72bd7088ea74bfe47f4c0a3
Matthew Matta <matthewlmatta@gmail.com>
2021-06-06 02:29:24 -0400

    added imports

--- client/src/Pages/Leaderboard.js
+++ client/src/Pages/Leaderboard.js
@@ -1,4 +1,8 @@
 import './Home.css';
+import './App.css';
+import Navbar from './components/navbar';
+import Home from './Pages/Home';
+import Leaderboard from './Pages/Leaderboard';
 import { Switch, Route } from 'react-router-dom';
 //import Login from './Pages/LoginForm';
 
