3c6b1e8d599e7b60b77dfe3f25961e47357b5586
yash.sheth30@gmail.com <shety2441@wrdsb.ca>
2021-06-05 22:17:53 -0400

    Added Pages

---
+++ client/src/Pages/Leaderboard.js
@@ -0,0 +1,14 @@
+import './home.css';
+
+function Home() {
+	return (
+		<div className="content home">
+			<h1>Graph Detailing Information?</h1>
+			<p className="info-paragraph">
+				This website is amazing
+			</p>
+		</div>
+	);
+}
+
+export default Home;
